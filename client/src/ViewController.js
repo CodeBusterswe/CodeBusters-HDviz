7d35748986f7c0c877c8396a91a266ef2df53299
Michele <michele.baldisseri@studenti.unipd.it>
2021-03-12 08:12:55 +0100

    aggiunti commenti

--- client/src/ViewController.js
+++ client/src/ViewController.js
@@ -7,9 +7,12 @@ const Controller = observer((props) => {
     /*
         il view controller viene usato per non far fare nessuna logica alla view
         solitamente per questo viene usato il viewmodel, ma in questo caso in cui i componenti usano anche gli stati 
-        (quindi anche un po di parte logica) viene utilizzato un controller specifico per la view
-        con questo controller posso quindi collegare click a una funzione del viewmodel, 
+        (quindi anche un po di parte logica) -> viene utilizzato un controller specifico per la view.
+        Con questo controller posso quindi collegare click a una funzione del viewmodel, 
         ma anche gestire lo stato della view in base ad una risposta della viewmodel
+        La vista non è a conoscenza del ViewModel e si basa sul ViewController per passare tutti i dati e gli eventi necessari.
+        La gestione dell'input dell'utente non deve essere lasciata al ViewModel ma piuttosto gestita qui nel ViewController 
+        che passerà dati puliti e preparati al ViewModel
     */
     
     const {viewModel} = props
