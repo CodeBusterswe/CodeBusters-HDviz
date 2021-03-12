65500ac9e3290ca7c469395065edcf7738300117
Paoloscia <56681786+Paoloscia@users.noreply.github.com>
2021-03-12 12:36:25 +0100

    cartelle assenti

--- client/src/components/UI/burgerMenuUI/BurgerMenu.js
+++ client/src/components/UI/burgerMenuUI/BurgerMenu.js
@@ -1,22 +1,34 @@
 import React from 'react'
-import { bubble as Menu } from 'react-burger-menu'
+import { elastic as Menu } from 'react-burger-menu'
+// try scaleRotate at the end (elastic) or slide
+import { CSVReader } from 'react-papaparse'
 
 const BurgerMenu = props => {
+  const {
+    handleOnDrop,
+    handleOnError,
+    handleOnRemoveFile
+  } = props
 
     return (
+
 <Menu {...props}>
 
+      <CSVReader id="MyCSVReader"
+        onDrop={handleOnDrop}
+        onError={handleOnError}
+        addRemoveButton
+        removeButtonColor='#ff0000'
+        onRemoveFile={handleOnRemoveFile}
+      >
+        <span>Drop CSV file here or click to upload.</span>
+      </CSVReader>
+
+
       <a className="menu-item" href="/burgers">
-        Burgerssssssssssss
+        ITEM DI PROva
       </a>
 
-      <a className="menu-item" href="/pizzas">
-        Pizzas
-      </a>
-
-      <a className="menu-item" href="/desserts">
-        Desserts
-      </a>
     </Menu>
     )
   }
