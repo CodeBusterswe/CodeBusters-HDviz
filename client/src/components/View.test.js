555e136189100f0daefc50be86c932cd52db46cf
zezze28 <62375818+zezze28@users.noreply.github.com>
2021-03-18 18:25:56 +0100

    view test

--- client/src/components/View.test.js
+++ client/src/components/View.test.js
@@ -1,6 +1,7 @@
 import { createEvent, fireEvent, getByRole, getByText, render, screen, waitFor } from "@testing-library/react";
 import { Modal } from "react-bootstrap";
 import App from "../App";
+
 test("Open web app", () => {
 	render(<App />);
 	expect(screen.getByRole('heading', { name: 'HDViz' })).toBeTruthy();
@@ -17,7 +18,6 @@ test("Open and close CSV modal", () => {
 		})
 	);
 	expect(event).toBeTruthy();
-	//fireEvent.click(event());
 	fireEvent.click(screen.getByRole('button', { name: 'Torna al menù' }));
 	const event2=jest.fn(
 		waitFor(() => {
@@ -26,8 +26,60 @@ test("Open and close CSV modal", () => {
 			).toBeTruthy()
 		})
 	);
+	console.log(event2);
+});
+test("Carica un csv",() => {
+	render(<App/>);
+	fireEvent.click(screen.getByRole('button',{name: 'Carica dati da CSV' }));
+	const event=jest.fn(
+		waitFor(() => {
+			(
+				screen.getByText('Carica un file CSV')
+			)
+		})
+	);
+	expect(event).toBeTruthy();
+	const file = new File(['(⌐□_□)'], './penguins.csv', {
+		type: 'csv',
+	  })
+	const inputEl = screen.getByText('Carica un file CSV');
+	fireEvent.drop(inputEl, {
+		DataTransfer:{files: [file]}
+	});
+  
+	/*fireEvent.drop(screen.getByText('Carica un file CSV'), {
+		DataTransfer: {
+			files: [new File(['(⌐□_□)'], 'penguins.csv', { type: 'text' })]
+		}
+	})*/
+	const event2=jest.fn(
+		waitFor(() => {
+			(
+				screen.getByRole('button',{name: 'Conferma selezione' })
+			)
+		})
+	);
 	expect(event2).toBeTruthy();
+	fireEvent.click(screen.getByRole('button',{name: 'Conferma selezione' }));
+	const event3=jest.fn(
+		waitFor(() => {
+			(
+				screen.getByRole('heading', { name: 'HDViz' })
+			).toBeTruthy()
+		})
+	);
+	/*expect(event3).toBeTruthy();
+	fireEvent.click(screen.getByRole('button',{name: 'Riduci dimensioni' }));
+	const event4=jest.fn(
+		waitFor(() => {
+			(
+				screen.getByRole('button', { name: 'Conferma selezione' })
+			).toBeTruthy()
+		})
+	);
+	console.log(event4);*/
 	screen.debug();
+
 });
 /*test("Open and close Riduci dimensioni", () => {
 	render(<App />);
