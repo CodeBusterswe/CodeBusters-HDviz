03a72962a31daf98e12977e9a0c091775c827ec5
GiacomoS <giacomo.sassaro@gmail.com>
2021-03-24 22:10:21 +0100

    Rimosso app.js

--- client/src/components/View.js
+++ client/src/components/View.js
@@ -1,16 +1,20 @@
 import React from "react";
 import Menu from "./UI/burgerMenuUI/Menu";
 import "./style.css";
+import "../App.css";
 import Header from "./UI/headerUI/Header";
 import Graph from "./UI/graphUI/Graph";
+import ViewModel from "../ViewModel";
+import { AppContextProvider } from "../ContextProvider";
 
 const View = () => {
+	const viewModel = new ViewModel();
 	return (
-		<div>
-			<Header />
-			<Menu />
+		<AppContextProvider value={viewModel}>
+			<Header/>
+			<Menu/>
 			<Graph/>
-		</div>
+		</AppContextProvider>
 	);
 };
 export default View;
