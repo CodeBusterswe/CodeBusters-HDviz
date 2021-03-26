5a029de19448575f72e9a62b08394cf74acd9d49
zezze28 <62375818+zezze28@users.noreply.github.com>
2021-03-26 18:38:10 +0100

    test but without neighbors

---
+++ client/src/components/test/DimensionalReduction.test.js
@@ -0,0 +1,7 @@
+import DimensionalReduction from "./../UI/burgerMenuUI/ModalContent/DimensionalReduction.js";
+/*
+test("riduzione dimensionale", async() => {
+	DimensionalReduction.handleChangeNeighbours(53);
+	const x=this.props.getNeighbors();
+	expect(x).toBe(53);
+});*/
