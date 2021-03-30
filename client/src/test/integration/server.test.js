0cbc17ae3031e96d149d68c3f3e730a06509dc06
Michele <michele.baldisseri@studenti.unipd.it>
2021-03-30 19:35:06 +0200

    server test to client

--- client/src/test/integration/server.test.js
+++ client/src/test/integration/server.test.js
@@ -1,24 +1,23 @@
 const request =require("supertest");
-const server = require('../server');
-
+const server = require("../../../../server/server");
 
 // start connection
 beforeAll(async () => {
-	server.address().port
+	// eslint-disable-next-line no-unused-expressions
+	server.address().port;
 	
 });
 
-describe("Server Running / ", () => {
+describe("Server Running /", () => {
 	test("It should respond with codeStatus 200", async () => {
 	  const response = await request(server).get("/test-server");
 	  //console.log(response.body)
 	  expect(response.body.server).toEqual(`server running ${server.address().port}`);
 	  expect(response.statusCode).toBe(200);
 	});
-  });
+});
 
 // close connectione 
 afterAll(async () => {
 	await server.close();
 });
-
