43b4290e836912bc8c9c5863ce7073b072d8c712
dude <hsn.safdari12@gmail.com>
2021-03-30 22:30:41 +0200

    add server test in client

--- server/__test__/server.test.js
+++ server/__test__/server.test.js
@@ -3,14 +3,12 @@ const server = require("../server");
 
 // start connection
 beforeAll(async () => {
-	server.address().port;
-	
+	server.address().port;	
 });
 
 describe("Server Running /", () => {
 	test("It should respond with codeStatus 200", async () => {
 	  const response = await request(server).get("/test-server");
-	  //console.log(response.body)
 	  expect(response.body.server).toEqual(`server running ${server.address().port}`);
 	  expect(response.statusCode).toBe(200);
 	});
