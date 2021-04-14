9fbe57d8a7d1a38a21d65f7e9604361af5a751bd
Michele <michele.baldisseri@studenti.unipd.it>
2021-04-14 18:39:16 +0200

    heatmap

--- client/src/components/UI/graphUI/charts/HmChart.js
+++ client/src/components/UI/graphUI/charts/HmChart.js
@@ -8,12 +8,21 @@ const HeatMap = observer(() => {
 
 	const {
 		renderChart,
+		rows,
+		columns,
+		heat
 	} = useInstance(new HmChartVM(useStore()));
     
-	useEffect(() => {renderChart();});
+	useEffect(() => {renderChart();}, [rows, columns, heat]);
 
 	return (
 		<div className="heatmap">
+			<svg className="plot" id="hm-svg">
+				<g>
+					<g className="axis" id="x-axis"/>
+					<g className="axis" id="y-axis"/>
+				</g>
+			</svg>
 		</div>
 	);
 
