74da110d6222504865485cc4bbc52ee6b37d9c4b
Paoloscia <56681786+Paoloscia@users.noreply.github.com>
2021-04-14 18:34:23 +0200

    porting other tests

---
+++ client/src/test/Dimension.unit.test.js
@@ -0,0 +1,16 @@
+import Dimension from "../stores/data/Dimension";
+
+let dimension = new Dimension("test");
+
+test("getters and setters", () => {
+	dimension.isChecked = true;
+	dimension.isNumeric = true;
+	dimension.isReduced = true;
+	dimension.toReduce = true;
+
+	expect(dimension.isChecked).toBe(true);
+	expect(dimension.isReduced).toBe(true);
+	expect(dimension.toReduce).toBe(true);
+	expect(dimension.isNumeric).toBe(true);
+	expect(dimension.value).toBe("test");
+});
