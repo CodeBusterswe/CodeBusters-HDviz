368574fad1db804404050a59e2390eccb4e3f9e7
Michele <michele.baldisseri@studenti.unipd.it>
2021-04-19 21:26:02 +0200

    update test

--- client/src/test/DimensionalReduction.unit.test.js
+++ client/src/test/DimensionalReduction.unit.test.js
@@ -11,14 +11,15 @@ let rootStore,
 	dataset;
 
 function reducedDim(value) { return value[0] === "test1" || value[0] === "test2";}
+function getData() {return rootStore.datasetStore.selectedData.map(row => Object.entries(row).filter(reducedDim));}
 
 describe("riduzione dimensionale", () => {
 
 	rootStore = new RootStore();
 	rootStore.datasetStore.reset();
-	dimension1 = new Dimension("peso");
-	dimension2 = new Dimension("altezza");
-	dataset = [{peso: 80, altezza: 175},{peso: 60, altezza: 162}];
+	dimension1 = new Dimension("sepal");
+	dimension2 = new Dimension("petal");
+	dataset = [{sepal: 5.1, petal: 3.5},{sepal: 4.9, petal: 3.0}];
 
 	beforeEach(()=>{
 
@@ -32,29 +33,56 @@ describe("riduzione dimensionale", () => {
 			</AppContextProvider>
 		);
 		fireEvent.click(screen.getByRole("button",{name: "Riduci dimensioni" }));	
-		fireEvent.change(screen.getByRole("textbox",{name:"New dimensions name"}),{target:{value: "test"}});
 	});
 
 	test("riduzione dimensionale fastmap", () => {
+		//set algorithm
+		fireEvent.change(screen.getByRole("textbox",{name:"New dimensions name"}),{target:{value: "test"}});
 		fireEvent.click(screen.getByRole("button",{name: "Start reduction" }));	
-		
-		let values = rootStore.datasetStore.selectedData.map(row => Object.entries(row).filter(reducedDim)),
+		//test new data
+		let values = getData(),
 			row1 = [values[0][0][1], values[0][1][1]],
 		    row2 = [values[1][0][1], values[1][1][1]];
-		expect(row1).toStrictEqual([23.853720883753123, 3.3717478808715233e-7]);
+		expect(row1).toStrictEqual([0.5385164807134502, 0]);
 		expect(row2).toStrictEqual([0, 0]);
 	});
 
 	test("riduzione dimensionale LLE", () => {
-
+		//set algorithm
+		fireEvent.change(screen.getByRole("combobox",{name:"Select algorithm"}),{target:{value:"lle"}});
+		fireEvent.change(screen.getByRole("textbox",{name:"New dimensions name"}),{target:{value: "test"}});
+		fireEvent.click(screen.getByRole("button",{name: "Start reduction" }));	
+		//test new data
+		let values = getData(),
+			row1 = [values[0][0][1], values[0][1][1]],
+		    row2 = [values[1][0][1], values[1][1][1]];
+		expect(row1).toStrictEqual([NaN, NaN]);
+		expect(row2).toStrictEqual([NaN, NaN]);
 	});
 
 	test("riduzione dimensionale IsoMap", () => {
-
+		//set algorithm
+		fireEvent.change(screen.getByRole("combobox",{name:"Select algorithm"}),{target:{value:"isoMap"}});
+		fireEvent.change(screen.getByRole("textbox",{name:"New dimensions name"}),{target:{value: "test"}});
+		fireEvent.click(screen.getByRole("button",{name: "Start reduction" }));	
+		//test new data
+		let values = getData(),
+			row1 = [values[0][0][1], values[0][1][1]],
+		    row2 = [values[1][0][1], values[1][1][1]];
+		expect(row1).toStrictEqual([-0.7071067811865475, 0.7071067811865475]);
+		expect(row2).toStrictEqual([0.7071067811865475, -0.7071067811865475]);
 	});
 
 	test("riduzione dimensionale t-SNE", () => {
-
+		//set algorithm
+		fireEvent.change(screen.getByRole("combobox",{name:"Select algorithm"}),{target:{value:"t-sne"}});
+		fireEvent.change(screen.getByRole("textbox",{name:"New dimensions name"}),{target:{value: "test"}});
+		fireEvent.click(screen.getByRole("button",{name: "Start reduction" }));	
+		//test new data
+		let values = getData(),
+			row1 = [values[0][0][1], values[0][1][1]],
+		    row2 = [values[1][0][1], values[1][1][1]];
+		expect(row1).toStrictEqual([-7.964809061175129, 56.35862642658786]);
+		expect(row2).toStrictEqual([7.964809061175129, -56.35862642658786]);
 	});
-
 });
