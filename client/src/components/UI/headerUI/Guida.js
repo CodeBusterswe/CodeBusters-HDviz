3eb592f1cbb9eda4ccebcd22d468e2d227022514
Giacomo <giacomo.sassaro@gmail.com>
2021-04-26 11:05:27 +0200

    corretti errori grammaticali guida e aggiunti link ai video

--- client/src/components/UI/headerUI/Guida.js
+++ client/src/components/UI/headerUI/Guida.js
@@ -65,12 +65,13 @@ function Guida(props) {
 					<ul>
 						<li><strong>Scatterplot Matrix</strong></li>
 						<li><strong>Adjency Matrix</strong></li>
+						<li><strong>Force Field</strong></li>
 						<li><strong>Heat Map</strong></li>
 						<li><strong>Linear Projection</strong></li>
 					</ul>
 					<h5>Utilizzo</h5>
 					<p>
-						Tutte le funzionalità di HDViz sono facilmente reperibili ed nel menu
+						Tutte le funzionalità di HDViz sono facilmente reperibili nel menu
 						laterale a scomparsa. <br/> <em>Inizialmente sono utilizzabili solo le prime voci:</em> 
 					</p>	
 					<div style={{display: "flex",flexDirection: "row",alignItems: "center"}}>
@@ -79,19 +80,19 @@ function Guida(props) {
 					</div>
 					<div style={{display: "flex",flexDirection: "row",alignItems: "center"}}>
 						<ImDatabase size={32} style={{marginLeft: "2em"}}/> 
-						<p style={{marginTop: "1em", marginLeft: "1em"}}>Carica dati dal DB</p>
+						<p style={{marginTop: "1em", marginLeft: "1em"}}><a class="linkTutorial" href="https://www.youtube.com/watch?v=MJ0WDG5yX-Y">Carica dati dal DB</a></p>
 					</div>
 					<div style={{display: "flex",flexDirection: "row",alignItems: "center"}}>
 						<FaFileCsv size={32} style={{marginLeft: "2em"}}/> 
-						<p style={{marginTop: "1em", marginLeft: "1em"}}>Carica dati da CSV</p>
+						<p style={{marginTop: "1em", marginLeft: "1em"}}><a class="linkTutorial" href="https://www.youtube.com/watch?v=7NIiFvb8XLY">Carica dati da CSV</a></p>
 					</div>
 					<p>
-						Tutte e tre aprono un modal per compiere le operazoni indicate. In particolare:
+						Tutte e tre aprono una finestra per compiere le operazioni indicate. In particolare:
 					</p>	
 					<ul>
-						<li>Il modal per il caricamento dei dati dal DB permette di scegliere uno dei dataset presenti nel DB 
+						<li>La finestra per il caricamento dei dati dal DB permette di scegliere uno dei dataset presenti nel DB 
 							ed effettuare immediatamente una selezione delle dimensioni da caricare;</li>	
-						<li>Il modal per il caricamento dei dati da file CSV permette anche la selezione delle dimensioni che si desidera
+						<li>La finestra per il caricamento dei dati da file CSV permette anche la selezione delle dimensioni che si desidera
 						utilizzare (<em>di default vengono utilizzate tutte quelle caricate</em>).</li>	
 					</ul>	
 					<p>
@@ -100,20 +101,20 @@ function Guida(props) {
 					</p>
 					<div style={{display: "flex",flexDirection: "row",alignItems: "center"}}>
 						<SiGraphcool size={32} style={{marginLeft: "2em"}}/> 
-						<p style={{marginTop: "1em", marginLeft: "1em"}}>Riduci dimensioni</p>
+						<p style={{marginTop: "1em", marginLeft: "1em"}}><a class="linkTutorial" target="_blank" rel="noreferrer" href="https://www.youtube.com/watch?v=HSNzSRYAfGc">Riduci dimensioni</a></p>
 					</div>
 					<div style={{display: "flex",flexDirection: "row",alignItems: "center"}}>
 						<AiOutlineFunction size={32} style={{marginLeft: "2em"}}/> 
-						<p style={{marginTop: "1em", marginLeft: "1em"}}>Calcola distanza</p>
+						<p style={{marginTop: "1em", marginLeft: "1em"}}><a class="linkTutorial" target="_blank" rel="noreferrer" href="https://www.youtube.com/watch?v=uEAzlijpLmM">Calcola distanza</a></p>
 					</div>
 					<p>
-						Anche in questo caso le voci apriranno un modal per compiere le operazioni indicate. In particolare:
+						Anche in questo caso le voci apriranno una finestra per compiere le operazioni indicate. In particolare:
 					</p>
 					<ul>
-						<li>Il modal per la riduzione dimensionale permette di scegliere inizialmente quali dimensioni utilizzare 
+						<li>La finestra per la riduzione dimensionale permette di scegliere inizialmente quali dimensioni utilizzare 
 							per la riduzione, poi quale algoritmo (<em>tra Fastmap, LLE, Isomap e TSNE</em>) e, in base a questa
 							ultima scelta una serie di parametri per eseguire la riduzione come più si preferisce;</li>
-						<li>Il modal per il calcolo della distanza permette di scegliere inizialmente
+						<li>La finestra per il calcolo della distanza permette di scegliere inizialmente
 							quali dimensioni utilizzare per effettuare il calcolo, poi quale funzione di distanza (<em>tra Euclidea, Camberra, Chebyshev e Manhattan</em>)
 							e il nome da dare alla nuova matrice delle distanze creata.
 						</li>
@@ -125,37 +126,37 @@ function Guida(props) {
 					</p>
 					<div style={{display: "flex",flexDirection: "row",alignItems: "center"}}>
 						<AiOutlineDotChart size={32} style={{marginLeft: "2em"}}/> 
-						<p style={{marginTop: "1em", marginLeft: "1em"}}>Scatterplot Matrix</p>
+						<p style={{marginTop: "1em", marginLeft: "1em"}}><a class="linkTutorial" target="_blank" rel="noreferrer" href="https://www.youtube.com/watch?v=1YtSDGRU8N8">Scatterplot Matrix</a></p>
 					</div>
 					<div style={{display: "flex",flexDirection: "row",alignItems: "center"}}>
 						<IoGrid size={32} style={{marginLeft: "2em"}}/> 
-						<p style={{marginTop: "1em", marginLeft: "1em"}}>Adjency Matrix</p>
+						<p style={{marginTop: "1em", marginLeft: "1em"}}><a class="linkTutorial" target="_blank" rel="noreferrer" href="https://www.youtube.com/watch?v=UQl7Rl9yRAQ">Adjency Matrix</a></p>
 					</div>
 					<div style={{display: "flex",flexDirection: "row",alignItems: "center"}}>
 						<RiMistFill size={32} style={{marginLeft: "2em"}}/> 
-						<p style={{marginTop: "1em", marginLeft: "1em"}}>Heat Map</p>
+						<p style={{marginTop: "1em", marginLeft: "1em"}}><a class="linkTutorial" target="_blank" rel="noreferrer" href="https://www.youtube.com/watch?v=GH6PCy3pm_k">Heat Map</a></p>
 					</div>
 					<div style={{display: "flex",flexDirection: "row",alignItems: "center"}}>
 						<IoShareSocialOutline size={32} style={{marginLeft: "2em"}}/> 
-						<p style={{marginTop: "1em", marginLeft: "1em"}}>Force Field</p>
+						<p style={{marginTop: "1em", marginLeft: "1em"}}><a class="linkTutorial" target="_blank" rel="noreferrer" href="https://www.youtube.com/watch?v=unLz7DA8gW0">Force Field</a></p>
 					</div>
 					<div style={{display: "flex",flexDirection: "row",alignItems: "center"}}>
 						<IoMoveSharp size={32} style={{marginLeft: "2em"}}/> 
-						<p style={{marginTop: "1em", marginLeft: "1em"}}>Linear Projection</p>
+						<p style={{marginTop: "1em", marginLeft: "1em"}}><a class="linkTutorial" target="_blank" rel="noreferrer" href="https://www.youtube.com/watch?v=PISKxPttkQ0">Linear Projection</a></p>
 					</div>
 					<p>
-						Una volta selezionata una di queste si aprirà una form sulla destra
+						Una volta selezionata una di queste si aprirà una finestra sulla destra
 						attraverso la quale sarà possibile modificare la visualizzazione del grafico, in termini di 
 						dimensioni da applicare agli assi, dimensione per l'applicazione del colore sui punti o,
-						per le visualizzazioni che ne fanno uso, è possibile scegliere la amtrice delle distanze da utilizzare tra quelle calcolate 
-						precedentemente. <em>Da notare come inizialmente tutti i campi siano settati a "No dimension".</em><br/>
-						Tale form è accompagnata da un bottone per nasconderla e centralizzare il grafico nello schermo
-						per concentrarsi solamente sull'analisi del grafico.
+						per le visualizzazioni che ne fanno uso, è possibile scegliere la matrice delle distanze da utilizzare tra quelle calcolate 
+						precedentemente. <em>Da notare come inizialmente tutti i campi siano impostati a "Nessuna dimensione".</em><br/>
+						Tale finestra è accompagnata da un pulsante per nasconderla e centrare il grafico nello schermo
+						per concentrarsi solamente sull'analisi dello stesso.
 					</p>
 					<h5>Conclusioni</h5>
 					<p>
 						Per ogni ulteriore chiarimento sull'utilizzo dell'applicazione è possibile contattarci 
-						all'e-mail codebuseterswe@gmail.com.
+						all'e-mail codebusterswe@gmail.com.
 					</p>
 				</>
 			</ModalBody>	
